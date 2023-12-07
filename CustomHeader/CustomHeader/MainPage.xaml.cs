87e46c57477ac123364baff7261db5b766fd0661
Rishvanth-Kumaresan <102664560+Rishvanth-Kumaresan@users.noreply.github.com>
2023-12-07 15:08:28 +0530

    Added sample.

---
+++ CustomHeader/CustomHeader/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Syncfusion.Maui.Picker;
+using System.Collections.ObjectModel;
+
+namespace CustomHeader;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+        ObservableCollection<object> cityName = new ObservableCollection<object>();
+        cityName.Add("UnitedStates");
+        cityName.Add("Qatar");
+        cityName.Add("India");
+        cityName.Add("Germany");
+        cityName.Add("Spain");
+        cityName.Add("Malaysia");
+        cityName.Add("Netherlands");
+        cityName.Add("Mexico");
+        cityName.Add("Brazil");
+        PickerColumn pickerColumn = new PickerColumn()
+        {
+            ItemsSource = cityName,
+            SelectedIndex = 4,
+        };
+        picker.Columns.Add(pickerColumn);
+    }
+}

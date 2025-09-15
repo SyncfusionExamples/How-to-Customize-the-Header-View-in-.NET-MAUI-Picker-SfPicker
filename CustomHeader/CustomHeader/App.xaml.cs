a4422e8fb9d4862f99109f600a4c8f72639cb1df
HadayaRahman-SF4673 <hadayarahman.m@syncfusion.com>
2025-09-15 11:10:32 +0530

    976803-UpdatedVersion

--- CustomHeader/CustomHeader/App.xaml.cs
+++ CustomHeader/CustomHeader/App.xaml.cs
@@ -1,11 +1,15 @@
-﻿namespace CustomHeader;
+﻿
+namespace CustomHeader;
 
 public partial class App : Application
 {
 	public App()
 	{
 		InitializeComponent();
-
-		MainPage = new MainPage();
 	}
+
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        return new Window(new MainPage());
+    }
 }
